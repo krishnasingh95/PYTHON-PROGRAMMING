8fe7a493275adefd81355a83c3fed6e036391528
Krishna Singh <krishnasa9664@gmail.com>
2026-06-24 13:59:11 +0530

    My thired commit

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-#zip   zip() combines multiple iterable element wise
-#syntax = zip(iterable1,iterable2....)
-
-names =["krishna","aman","sakshi"]
-ages=[21,22,34]
-result=zip(names,ages)
-print(list(result))
